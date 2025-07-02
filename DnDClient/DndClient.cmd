299f472119aff389e59dd58a75dbf3124d3aa59a
chowizard <chowizard@hotmail.com>
2025-07-03 01:20:05 +0900

    update : Update client running cmd script.
    - If not found python venv, install venv and required pakages first.

--- DnDClient/DndClient.cmd
+++ DnDClient/DndClient.cmd
@@ -1,6 +1,17 @@
 @ECHO OFF
 
-CALL ..\.venv\Scripts\activate.bat
+SET VenvPath=..\.venv
+SET PackageListFilePath=..\requirements.txt
+
+IF NOT EXIST %VenvPath% (
+    ECHO Install python virtual environment...
+    python -m venv %VenvPath%
+    CALL %VenvPath%\Scripts\activate.bat
+    pip install -r %PackageListFilePath%
+) ELSE (
+    CALL %VenvPath%\Scripts\activate.bat
+)
+
 python .\Main.py
 
 PAUSE
